09ac3f8d1d29c82244bc8a91ef126aa7a5030b37
Sanam-Ukey <sanamukey02@gmail.com>
2026-01-24 11:11:13 +0530

    Create testproject.groovy

---
+++ testproject.groovy
@@ -0,0 +1,28 @@
+pipeline {
+    agent { label 'slave' }
+    stages {
+        stage('git-pull-stage') {
+            steps {
+                git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'
+            }
+        }
+         stage('build-stage') {
+            steps {
+                sh '/opt/maven/bin/mvn clean package'
+            }
+        }
+         stage('test-stage') {
+            steps {
+                sh '''mvn sonar:sonar \\
+                      -Dsonar.projectKey=studentapp \\
+                      -Dsonar.host.url=http://172.31.20.233:9000 \\
+                      -Dsonar.login=2f1647db16a07409328b26dda5c263e0a9d5f543'''
+            }
+        }
+         stage('deploy-stage') {
+            steps {
+                echo 'code deploy sucessfully'
+            }
+        }
+    }
+}
