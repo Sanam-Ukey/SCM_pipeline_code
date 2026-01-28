888c16831753199ebbece98afded0a0e96635feb
Sanam-Ukey <sanamukey02@gmail.com>
2026-01-28 20:06:47 +0530

    store artifactory in s3

---
+++ artifactory.groovy
@@ -0,0 +1,45 @@
+pipeline {
+    agent { label 'slave' }
+    stages {
+        stage('git-pull-stage') {
+            steps {
+                git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'
+            }
+        }
+         stage('build-stage') {
+            steps {
+                sh '/opt/maven/bin/mvn clean package'
+            }
+        }
+         stage('tesr-stage') {
+            steps {
+                withSonarQubeEnv(installationName: 'sonar',credentialsId: 'sonar-cred') {
+                 sh ' /opt/maven/bin/mvn clean verify sonar:sonar '
+            }
+                // sh '''  /opt/maven/bin/mvn clean verify sonar:sonar \\
+                //         -Dsonar.projectKey=new-studentapp \\
+                //         -Dsonar.host.url=http://172.31.21.29:9000 \\
+                //         -Dsonar.login=1bdbff4bf01b412d86dd2e9aaa23cff101b5c927'''
+            }
+        }
+         stage('Quality_Gate') {
+            steps {
+                timeout(2) {
+   
+            }
+                waitForQualityGate true
+            }
+        }
+           stage('Artifatory-stage') {
+            steps {
+               sh 'aws s3 cp target/studentapp-2.2-SNAPSHOT.war s3://bucket-artifact-0987/'
+            }
+        }
+
+         stage('deploy-stage') {
+            steps {
+                echo 'code deploy sucessfully'
+            }
+        }
+    }
+}
