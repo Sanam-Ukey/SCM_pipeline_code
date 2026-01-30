3f350efb55fc5c82e619796a0c7ba09437bd1ebe
Sanam-Ukey <sanamukey02@gmail.com>
2026-01-30 16:24:44 +0530

    Create deploy stage

---
+++ deploy.groovy
@@ -0,0 +1,41 @@
+pipeline {
+    agent { label 'slave' }
+
+    stages {
+
+        stage('git-pull-stage') {
+            steps {
+                git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'
+            }
+        }
+
+        stage('build-stage') {
+            steps {
+                sh '/opt/maven/bin/mvn clean package'
+            }
+        }
+
+        stage('test-stage') {
+            steps {
+                withSonarQubeEnv(installationName: 'sonar', credentialsId: 'sonar-cred') {
+                    sh '/opt/maven/bin/mvn clean verify sonar:sonar'
+                }
+            }
+        }
+
+        stage('Quality_Gate') {
+            steps {
+                timeout(2) {
+   
+                }
+                waitForQualityGate true
+            }
+        }
+
+        stage('deploy-stage') {
+            steps {
+                deploy adapters: [tomcat9(alternativeDeploymentContext: '', credentialsId: 'tomcat-cred', path: '', url: 'http://54.169.146.128:8080/')], contextPath: '/', war: '**/*.war'
+            }
+        }
+    }
+}
